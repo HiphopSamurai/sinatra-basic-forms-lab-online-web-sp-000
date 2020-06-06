7bc3ad4e433788b899c5e30f060b7f52fe1653a9
Garrett Thompson <gtcruz.gt@gmail.com>
2020-06-06 04:19:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  # In app.rb build out a GET request to load a home page. 
+  # The home page should go to the main route /.
 end
