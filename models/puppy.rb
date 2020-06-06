0cd67d555f9d50155c330cecaa22b7c2f529c46b
Garrett Thompson <gtcruz.gt@gmail.com>
2020-06-06 04:18:31 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,8 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy
-  #Puppies should have name, breed, and age attributes. 
-  #You will need to be able to pass these three 
-  #attributes to initialization, as well as readers 
+  #Puppies should have name, breed, and age attributes.
+  #You will need to be able to pass these three
+  #attributes to initialization, as well as readers
   #or accessors for the attributes.
 
 end
